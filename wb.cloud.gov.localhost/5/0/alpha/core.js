952075fbb5b800cd4949e0e3b4a4583cebeaf1d1
masterbee <mario@beelabs.ca>
2018-07-03 21:54:38 -0400

    added more sample logic

---
+++ wb.cloud.gov.localhost/5/0/alpha/core.js
@@ -0,0 +1,6 @@
+
+let wb = {
+    version : "5.0-alpha",
+    timer : function ( selector, options ) { this.innerHTML += " (OK! Timer)" }
+};
+
--- wb.cloud.gov.localhost/5/0/alpha/index.js
+++ wb.cloud.gov.localhost/5/0/alpha/index.js
@@ -1,3 +1,25 @@
-/*
-  New JS Logic
+/**
+ *  WET-BOEW 5 alpha
+ *  @author: Government  of Canada
+ *  @licence: MIT
  */
+(function( doc, win, undef )
+{
+
+
+    <!--#include virtual="core.js" -->
+
+    let nodes = doc.querySelectorAll("[data-wb5]");
+
+    for (let elem of nodes)
+    {
+        let [ command, selector, options ] = elem.dataset.wb5.split( "@" );
+
+        if ( command )
+        {
+            wb[ command ].call( elem, selector, options );
+        }
+    }
+
+
+})(  document, window )
